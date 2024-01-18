f85d1e0139c6d0c8e71910aa6786919b530a465d
Deepesh Nimma <deepnimma@gmail.com>
2024-01-17 22:11:51 -0500

    css stuff for socials buttons and add description

--- src/sections/body.tsx
+++ src/sections/body.tsx
@@ -2,7 +2,8 @@ import '../css/Body.css'
 
 const Body: React.FC = () => {
     return <div className="body">
-        <span className="bodyText">Deepesh Nimma</span>
+        <span className="nameText">Deepesh Nimma</span>
+        <span className="desc">A silly little guy doing silly little things in this silly little world.</span>
     </div>
 };
 
