8fcc206a55e001548ea63816620196e4c482a81c
Deepesh Nimma <deepnimma@gmail.com>
2024-03-16 18:47:27 -0400

    basic investigates page

--- src/sections/pages/investigates.tsx
+++ src/sections/pages/investigates.tsx
@@ -1,9 +1,10 @@
+import '../../css/pages/Investigates.css'
 import React from "react";
 
 const Investigates: React.FC = () => {
     return (
-        <div>
-            <h1>This is the investigates page.</h1>
+        <div className="textHolder">
+            <span className="investigatesText">Deepesh Nimma Investigates.</span>
         </div>
     ) // return
 } // Investigates
