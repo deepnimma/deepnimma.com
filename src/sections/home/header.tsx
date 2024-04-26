e8118fa358b2835b72610f3f7585d3290197cc83
Deepesh Nimma <deepnimma@gmail.com>
2024-04-26 13:56:44 -0400

    remove unused variable

--- src/sections/home/header.tsx
+++ src/sections/home/header.tsx
@@ -16,26 +16,6 @@ const NavBar: React.FC = () => {
     </div>
 }; // NavBar
 
-const SocialButtons: React.FC = () => {
-    return <div className="socials">
-        <a href={githubBtn.link} target="_blank" rel="noreferrer">
-            <button className="githubBtn">
-                <img className="actualGithubBtn" src={githubBtn.iconLocation} alt={githubBtn.altText} />
-            </button>
-        </a>
-        <a href={linkedInBtn.link} target="_blank" rel="noreferrer">
-            <button className="linkedinBtn">
-                <img className="actualLinkedinBtn" src={linkedInBtn.iconLocation} alt={linkedInBtn.altText} />
-            </button>
-        </a>
-        <a href={instaBtn.link} target="_blank" rel="noreferrer">
-            <button className="instagramBtn">
-                <img className="actualInstagramBtn" src={instaBtn.iconLocation} alt={instaBtn.altText} />
-            </button>
-        </a>
-    </div>
-}; // socialsButtons
-
 const Header: React.FC = () => {
     return <div className="header">
         <div className={"navBar"}>
