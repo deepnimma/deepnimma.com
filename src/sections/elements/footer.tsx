e063a81b20d4813a31084bb4b81a7ea28c2a7ab8
Deepesh Nimma <deepnimma@gmail.com>
2024-04-27 00:21:45 -0400

    trying my best

---
+++ src/sections/elements/footer.tsx
@@ -0,0 +1,50 @@
+import '../../css/elements/Footer.css';
+import { tabs } from "../../objects/footerNavBar"
+import {githubBtn, instaBtn, linkedInBtn} from "../../objects/socialsbuttons";
+
+const NavBar: React.FC = () => {
+    return (
+        <div className={"pages"}>
+            <a href={tabs[0].link} rel={"noopener noreferrer"}>
+                <button className={tabs[0].name}>{tabs[0].name}</button>
+            </a>
+            <a href={tabs[1].link} rel={"noopener noreferrer"}>
+                <button className={tabs[1].name}>{tabs[1].name}</button>
+            </a>
+            <a href={tabs[2].link} rel={"noopener noreferrer"}>
+                <button className={tabs[2].name}>{tabs[2].name}</button>
+            </a>
+        </div>
+    ) // return
+} // NavBar
+
+const SocialButtons: React.FC = () => {
+    return <div className="socials">
+        <a href={githubBtn.link} target="_blank" rel="noreferrer">
+            <button className="githubBtn">
+                <img className="actualGithubBtn" src={githubBtn.iconLocation} alt={githubBtn.altText} />
+            </button>
+        </a>
+        <a href={linkedInBtn.link} target="_blank" rel="noreferrer">
+            <button className="linkedinBtn">
+                <img className="actualLinkedinBtn" src={linkedInBtn.iconLocation} alt={linkedInBtn.altText} />
+            </button>
+        </a>
+        <a href={instaBtn.link} target="_blank" rel="noreferrer">
+            <button className="instagramBtn">
+                <img className="actualInstagramBtn" src={instaBtn.iconLocation} alt={instaBtn.altText} />
+            </button>
+        </a>
+    </div>
+}; // socialsButtons
+
+const Footer: React.FC = () => {
+    return (
+        <div className={"elementsfooter"}>
+            <SocialButtons />
+            <NavBar />
+        </div>
+    ) // return
+} // Mainpagefooter
+
+export { Footer }
