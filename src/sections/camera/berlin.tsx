22092de16f95e754e74fe75882c70a08607207f4
Deepesh Nimma <deepnimma@gmail.com>
2024-04-29 23:09:02 -0400

    create camera sections for all places

---
+++ src/sections/camera/berlin.tsx
@@ -0,0 +1,35 @@
+import { Header } from '../elements/header'
+import { Footer } from '../elements/footer'
+import {Box, ImageList, ImageListItem} from "@mui/material";
+import {berlin as pic_names} from "../elements/namefactory";
+import React from "react";
+
+const Masonry: React.FC = () => {
+    return (
+        <Box sx={{overflowY: 'scroll'}}>
+            <ImageList variant={"masonry"} cols={3} gap={8}>
+                {pic_names.map((num) => (
+                    <ImageListItem key={num}>
+                        <img
+                            src={require(`../../data/portfolio/2023/berlin/${num}.jpg`)}
+                            alt={num}
+                            loading={"lazy"}
+                            />
+                    </ImageListItem>
+                ))}
+            </ImageList>
+        </Box>
+    )
+} // ImageList
+
+const Berlin: React.FC = () => {
+    return (
+        <div>
+            <Header />
+            <Masonry />
+            <Footer />
+        </div>
+    )
+}
+
+export default Berlin
