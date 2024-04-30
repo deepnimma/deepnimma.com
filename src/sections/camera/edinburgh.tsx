b9b2843f07d2870eb292d63dde7cbfb2bee66830
Deepesh Nimma <deepnimma@gmail.com>
2024-04-29 23:02:05 -0400

    add missing edinburgh picture and fix edinburgh page

--- src/sections/camera/edinburgh.tsx
+++ src/sections/camera/edinburgh.tsx
@@ -11,7 +11,7 @@ const Masonry: React.FC = () => {
                 {pic_names.map((num) => (
                     <ImageListItem key={num}>
                         <img
-                            src={require(`../../data/portfolio/2023/edinburgh/edinburgh-${num}.jpg`)}
+                            src={require(`../../data/portfolio/2023/edinburgh/${num}.jpg`)}
                             alt={num}
                             loading={"lazy"}
                             />
