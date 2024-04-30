a164cb29e436d1d4b9e493a13634aebf18ebbd82
Deepesh Nimma <deepnimma@gmail.com>
2024-04-29 23:46:29 -0400

    fix duplicate images and issues with names

--- src/sections/elements/namefactory.tsx
+++ src/sections/elements/namefactory.tsx
@@ -17,13 +17,13 @@ const belgrade: string[] = nameFactory("belgrade", 1, 15);
 const berlin: string[] = nameFactory("berlin", 1, 16);
 const budapest: string[] = nameFactory("budapest", 1, 6);
 const copenhagen: string[] = nameFactory("copenhagen", 1, 15);
-const dublin: string[] = nameFactory("dublin", 1, 23);
+const dublin: string[] = nameFactory("dublin", 1, 22);
 const edinburgh: string[] = nameFactory("edinburgh", 26, 49);
-const london: string[] = nameFactory("london", 1, 19);
-const miscellaneous: string[] = nameFactory("misc", 1, 16);
+const london: string[] = nameFactory("london", 1, 18);
+const miscellaneous: string[] = nameFactory("misc", 1, 11);
 const reykjavik: string[] = nameFactory("reykjavik", 1, 15);
 const rome: string[] = nameFactory("rome", 1, 16);
-const stockholm: string[] = nameFactory("stockholm", 1, 14);
+const stockholm: string[] = nameFactory("stockholm", 1, 13);
 const vatican: string[] = nameFactory("vatican", 1, 12);
 
 export {
