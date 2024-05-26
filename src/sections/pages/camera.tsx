c2feceb37b3e2929ae70cac541965877a53beaac
Deepesh Nimma <deepnimma@gmail.com>
2024-05-26 14:06:43 +0530

    fix hidden overlap on Camera page

--- src/sections/pages/camera.tsx
+++ src/sections/pages/camera.tsx
@@ -7,24 +7,26 @@ import {TwentyThreeTitle, TwentyFourTitle} from "./cameraSubsections/titles";
 
 const Camera: React.FC = () => {
     return (
-        <Grid2 container rowSpacing={8} columnSpacing={3} columns={1} className={"cameraConCon"}>
-            <Grid2>
-                <Grid2 xs display={"flex"} justifyContent={"center"} alignItems={"center"}>
-                    <TwentyFourTitle />
+        <div className={"gridcon"}>
+            <Grid2 container rowSpacing={8} columnSpacing={3} columns={1} className={"cameraConCon"}>
+                <Grid2>
+                    <Grid2 xs display={"flex"} justifyContent={"center"} alignItems={"center"}>
+                        <TwentyFourTitle />
+                    </Grid2>
+                    <Grid2 xs display={"flex"} justifyContent={"center"}>
+                        <TwentyFourPics />
+                    </Grid2>
                 </Grid2>
-                <Grid2 xs display={"flex"} justifyContent={"center"}>
-                    <TwentyFourPics />
+                <Grid2>
+                    <Grid2 xs display={"flex"} justifyContent={"center"} alignItems={"center"}>
+                        <TwentyThreeTitle />
+                    </Grid2>
+                    <Grid2 xs display={"flex"} justifyContent={"center"}>
+                        <TwentyThreePics />
+                    </Grid2>
                 </Grid2>
             </Grid2>
-            <Grid2>
-                <Grid2 xs display={"flex"} justifyContent={"center"} alignItems={"center"}>
-                    <TwentyThreeTitle />
-                </Grid2>
-                <Grid2 xs display={"flex"} justifyContent={"center"}>
-                    <TwentyThreePics />
-                </Grid2>
-            </Grid2>
-        </Grid2>
+        </div>
     )
 } // FullGrid
 
