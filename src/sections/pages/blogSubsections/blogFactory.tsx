f84d5cb0d225937a33f6a3f6d6ede3d4e8713cf9
Deepesh Nimma <deepnimma@gmail.com>
2024-06-01 10:04:38 +0530

    blog list shows up

--- src/sections/pages/blogSubsections/blogFactory.tsx
+++ src/sections/pages/blogSubsections/blogFactory.tsx
@@ -1,1 +1,36 @@
-export default {}
+import {Card, CardActionArea, CardContent, CardMedia, Typography} from "@mui/material";
+
+interface BlogCardProps {
+    title: string;
+    description: string;
+    date: string;
+}
+
+const BlogCardFactory: React.FC<BlogCardProps> = ({ title , description, date}) => {
+    return (
+        <Card
+            sx={{
+                minWidth: "85vw",
+                maxWidth: "85vw",
+                position: "relative",
+                color: "darkgrey",
+                padding: 3
+            }}>
+            <CardActionArea>
+                <CardContent>
+                    <Typography gutterBottom variant={"h4"} color="black" component={"div"} fontFamily={"Bahnschrift"}>
+                        {title}
+                    </Typography>
+                    <Typography variant={"h5"} fontFamily={"Bahnschrift"}>
+                        {description}
+                    </Typography>
+                    <Typography variant={"body1"} color={"text.secondary"} fontFamily={"Sudo"}>
+                        {date}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+    );
+}
+
+export default BlogCardFactory
--- src/sections/pages/investigates.tsx
+++ src/sections/pages/investigates.tsx
@@ -1,12 +1,24 @@
 import '../../css/pages/Investigates.css'
 import React from "react";
-import {postData} from "./../../util/jsonLoader";
+
+import {default as first_post} from "./../../data/blog/first_post.json"
+import BlogCardFactory from "./blogSubsections/blogFactory";
+import {Stack} from "@mui/material";
 
 const Investigates: React.FC = () => {
     return (
-        <div className="investigatesCon">
-            <span className="investigatesText">${postData[0].title}</span>
-        </div>
+        <Stack spacing={2} alignItems={"center"} padding={"20px"}>
+            <BlogCardFactory
+                title={"MY Name is Deepesh Nimma"}
+                description={"And this is my blog"}
+                date={"June 1st, 2024"}
+                />
+            <BlogCardFactory
+                title={first_post.title}
+                description={first_post.description}
+                date={first_post.date}
+            />
+        </Stack>
     ) // return
 } // Investigates
 
