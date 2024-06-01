0804cd7ab3b4a677235282ccdb9de52a811cc0c3
Deepesh Nimma <deepnimma@gmail.com>
2024-06-01 09:37:46 +0530

    blog factory file

---
+++ src/sections/pages/blogSubsections/june_23/first_post.tsx
@@ -0,0 +1,3 @@
+import * as blog_post from "./../../../../data/blog/first_post.json"
+
+export default {}
--- src/util/jsonLoader.tsx
+++ src/util/jsonLoader.tsx
@@ -1,4 +1,5 @@
-import fs from 'fs'
+// import fs from 'fs'
+import * as index from "./../data/blog/index.json"
 
 interface Posts {
     title: string,
@@ -11,17 +12,4 @@ interface Posts {
     references: string[]
 } // Posts
 
-function readJsonFiles(): Posts[] {
-    const indexString: string = fs.readFileSync("./../data/blog/index.json", "utf-8");
-    const indexData = JSON.parse(indexString);
-    let data: Posts[] = Array(0);
-
-    for (let i = 0; i < indexData.index.length; i++) {
-        let dataString = fs.readFileSync(`./../data/blog/${indexData.index[i]}`, "utf-8");
-        data.push(JSON.parse(dataString));
-    } // for
-
-    return data;
-} // readJsonFiles
-
-export const postData = readJsonFiles();
+export default {}
